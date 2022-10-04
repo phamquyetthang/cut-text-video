777e9a12cb298cea3950d5bb7d9bed41fe93d901
phamquyetthang <quyetthang.phucchi@gmail.com>
2022-10-04 10:29:10 +0700

    fix bug notification

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import VueDraggableResizable from "vue-draggable-resizable";
 import "vue-draggable-resizable/dist/VueDraggableResizable.css";
+import Notifications from "vue-notification";
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 global.jQuery = require('jquery');
 var $ = global.jQuery;
 window.$ = $;
 
+Vue.use(Notifications);
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2, { heightAuto: false, confirmButtonText: `<span style="padding: 0 10px">OK</span>` });
 Vue.component("vue-draggable-resizable", VueDraggableResizable);
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? "/REPO_NAME/" : "/"
+  publicPath: process.env.NODE_ENV === 'production' ? "/cut-text-video/" : "/"
 })
